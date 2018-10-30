57bdc1a123c8f774d37df38ed2a49d354b3d8c43
Flatiron School <andrewsprague@outlook.com>
2018-10-30 15:14:02 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,63 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleUserNameChange = event => {
+    let newUserName = this.state.username;
+    newUserName = event.target.value;
+    this.setState({ username: newUserName }, () =>
+      console.log(this.state.username)
+    );
+  };
+
+  handlePasswordChange = event => {
+    let newPassword = this.state.password;
+    newPassword = event.target.value;
+    this.setState({ password: newPassword });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    let username = this.state.username;
+    let password = this.state.password;
+    if (username.length > 0 && password.length > 0) {
+      this.props.onSubmit({ username: username, password: password });
+    }
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input
+              id="username"
+              name="username"
+              type="text"
+              value={this.state.username}
+              onChange={this.handleUserNameChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.handlePasswordChange}
+            />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  handleMessageChange = event => {
+    let newMessage = this.state.message;
+    newMessage = event.target.value;
+    this.setState({ message: newMessage }, () =>
+      console.log(this.state.message)
+    );
+  };
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          name="message"
+          value={this.state.message}
+          onChange={this.handleMessageChange}
+        />
+        <strong>
+          Characters left: {this.props.maxChars - this.state.message.length}
+        </strong>
       </div>
     );
   }
